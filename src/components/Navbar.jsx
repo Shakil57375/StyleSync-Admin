355aecd1c3408825eb97e04ca763d22ac9c31fd5
Shakil57375 <shakil57375@gmail.com>
2024-11-19 22:07:10 +0600

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { assets } from "../assets/assets"
 const Navbar = () => {
   return (
     <div>
-        <img src={assets.logo} alt="" />
+        <img className="w-[80px]" src={assets.logo} alt="" />
         <button>Logout</button>
     </div>
   )
